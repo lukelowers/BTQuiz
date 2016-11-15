6a3f5da604b69464103dd5b64dd095500cc10a9d
andrewjamen <ajamen@ilstu.edu>
2016-11-15 15:18:38 -0600

    connect activites

--- app/src/main/java/com/example/luke/btquiz/MainActivity.java
+++ app/src/main/java/com/example/luke/btquiz/MainActivity.java
@@ -2,12 +2,51 @@ package com.example.luke.btquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    int i = 0;
+
+
+    private Button student;
+    private Button faculty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        activitySwitch();
+    }
+
+    public void activitySwitch(){
+        student = (Button) findViewById(R.id.studentButton);
+        faculty = (Button) findViewById(R.id.facultyButton);
+
+        student.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, StudentConnect.class);
+                startActivity(intent);
+            }
+        });
+
+        faculty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, FacultyConnect.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
 }
