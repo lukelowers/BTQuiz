1b23a9e0900624d98cdbd059d10e8c65cb108434
andrewjamen <ajamen@ilstu.edu>
2016-11-28 12:41:18 -0600

    permissions in manifest

--- app/src/main/java/com/example/luke/btquiz/CreateQuiz.java
+++ app/src/main/java/com/example/luke/btquiz/CreateQuiz.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 public class CreateQuiz extends AppCompatActivity {
 
-    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/luke/btquiz/FacultyConnect.java
+++ app/src/main/java/com/example/luke/btquiz/FacultyConnect.java
@@ -2,12 +2,26 @@ package com.example.luke.btquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 
 public class FacultyConnect extends AppCompatActivity {
+
+    private final static int REQUEST_ENABLE_BT = 1;
+    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faculty_connect);
     }
+
+    if (!mBluetoothAdapter.isEnabled()) {
+        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+    }
+
+
+
+
 }
